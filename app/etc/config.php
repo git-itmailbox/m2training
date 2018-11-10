154c84e7ecc66e05b52bd71d4a7c8e869465a7bf
yura <itmailbox@i.ua>
2018-11-10 19:40:34 +0200

    add Test plugin for UrlInterface

--- app/etc/config.php
+++ app/etc/config.php
@@ -128,6 +128,7 @@ return [
         'Training_TestOM' => 1,
         'Training_Dependency2' => 0,
         'Training_Dependency' => 1,
+        'Training_Test' => 1,
         'Training_AdditionalTestOM' => 1,
         'Vertex_Tax' => 1
     ]
