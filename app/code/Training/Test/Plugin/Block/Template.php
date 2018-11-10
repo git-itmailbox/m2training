b892b60f45955bd2a39102a9a7faf3d503d8aee0
yura <itmailbox@i.ua>
2018-11-10 21:36:59 +0200

    1.8 (1-2) Added plugin for each View Element

---
+++ app/code/Training/Test/Plugin/Block/Template.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Training\Test\Plugin\Block;
+
+class Template
+{
+    public function afterToHtml(\Magento\Framework\View\Element\Template $subject, $result )
+    {
+        $result = '<div><p>!!!</p><p>' . $subject->getTemplate() . '</p>'
+            . '<p>' . get_class($subject) . '</p>' . $result . '</div>';
+        return $result;
+    }
+}
