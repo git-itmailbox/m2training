f868e7188516b0fb9a4cda1a868c8ce6ac7ad5e1
yura <itmailbox@i.ua>
2018-11-13 18:25:07 +0200

    2.4 (3) Modified controller, now it return resultRaw object

--- app/code/Training/Test/Controller/Index/Index.php
+++ app/code/Training/Test/Controller/Index/Index.php
@@ -2,10 +2,27 @@
 
 namespace Training\Test\Controller\Index;
 
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\Controller\Result\RawFactory;
+
 class Index extends \Magento\Framework\App\Action\Action
 {
+
+    private $resultRawFactory;
+
+    public function __construct(Context $context, RawFactory $resultRawFactory)
+    {
+        $this->resultRawFactory = $resultRawFactory;
+        parent::__construct($context);
+    }
+
     public function execute()
     {
-        $this->getResponse()->appendBody('simple text');
+        $resultRaw = $this->resultRawFactory->create();
+        $resultRaw->setContents('<h2>simple text</h2>');
+        //just for fun test
+        $resultRaw->setStatusHeader(402);
+
+        return $resultRaw;
     }
 }
