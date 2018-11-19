93b9503ce5d360619ac7f601148ad3be0c8af54e
yura <itmailbox@i.ua>
2018-11-19 20:34:14 +0200

    3.7 set custom template for Description Block

--- app/code/Training/Test/Block/Product/View/Description.php
+++ app/code/Training/Test/Block/Product/View/Description.php
@@ -7,7 +7,8 @@ class Description extends \Magento\Catalog\Block\Product\View\Description
     public function beforeToHtml(
         \Magento\Catalog\Block\Product\View\Description $subject
     ) {
-        $subject->getProduct()->setDescription('Test description');
+//        $subject->getProduct()->setDescription('Test description');
+        $subject->setTemplate('Training_Test::description.phtml');
     }
 
 }
