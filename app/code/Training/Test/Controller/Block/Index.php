7d84ba39a72f479a39de32900bffbca55769d2e2
yura <itmailbox@i.ua>
2018-11-19 15:10:43 +0200

    3.2 output Block with resultRaw object

--- app/code/Training/Test/Controller/Block/Index.php
+++ app/code/Training/Test/Controller/Block/Index.php
@@ -2,6 +2,9 @@
 
 namespace Training\Test\Controller\Block;
 
+use Magento\Framework\Controller\Result\Raw;
+use Magento\Framework\Controller\Result\RawFactory;
+
 class Index extends \Magento\Framework\App\Action\Action
 {
     /**
@@ -10,21 +13,33 @@ class Index extends \Magento\Framework\App\Action\Action
     private $layoutFactory;
 
     /**
+     * @var RawFactory
+     */
+    private $resultRawFactory;
+
+    /**
      * @param \Magento\Backend\App\Action\Context $context
      * @param \Magento\Framework\View\LayoutFactory $layoutFactory
      */
     public function __construct(
         \Magento\Backend\App\Action\Context $context,
-        \Magento\Framework\View\LayoutFactory $layoutFactory
+        \Magento\Framework\View\LayoutFactory $layoutFactory,
+        RawFactory $resultRawFactory
     ) {
         $this->layoutFactory = $layoutFactory;
+        $this->resultRawFactory = $resultRawFactory;
         parent::__construct($context);
     }
 
     public function execute()
     {
+        /*  @var  $resultRaw Raw */
+        $resultRaw = $this->resultRawFactory->create();
+
         $layout = $this->layoutFactory->create();
         $block  = $layout->createBlock('Training\Test\Block\Test');
-        $this->getResponse()->appendBody($block->toHtml());
+        $resultRaw->setContents($block->toHtml());
+        return $resultRaw;
     }
+
 }
