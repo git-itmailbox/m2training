582e5f83abb08a4a4cd7e715b5ea124185f42cfe
yura <itmailbox@i.ua>
2018-11-20 21:56:54 +0200

    3.10 set title by page configuration xml file && disable NoRouteHandler

---
+++ app/code/Training/Render/Controller/Layout/Onecolumn.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Training\Render\Controller\Layout;
+
+class Onecolumn extends \Magento\Framework\App\Action\Action
+{
+    /**
+     * @var \Magento\Framework\View\Result\PageFactory
+     */
+    private $resultPageFactory;
+
+    /**
+     * @param \Magento\Backend\App\Action\Context $context
+     * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
+     */
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+    ) {
+        $this->resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
+
+    }
+    public function execute()
+    {
+        return $this->resultPageFactory->create();
+    }
+}
