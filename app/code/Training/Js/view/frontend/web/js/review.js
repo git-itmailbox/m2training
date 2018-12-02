2792ee2ced16c3b5f02ba5705cc017811b294328
yura <itmailbox@i.ua>
2018-12-02 21:57:33 +0200

    4.4 Random review on each page with "next review" button and ajax updating

---
+++ app/code/Training/Js/view/frontend/web/js/review.js
@@ -0,0 +1,36 @@
+define([
+    'uiComponent',
+    'jquery',
+    'ko'
+], function (Component, $, ko) {
+    'use strict';
+    return Component.extend({
+        reviewerName: ko.observable(''),
+        reviewerMessage: ko.observable(''),
+        isLoading: ko.observable(false),
+        url: '',
+        initialize: function () {
+            this._super();
+            this.nextReview();
+            return this;
+        },
+        nextReview: function () {
+            this.isLoading(true);
+            var self = this;
+            $.ajax({
+                url: self.url,
+                type: 'post',
+                dataType: 'json'})
+                .done(function (data) {
+                    var dataJson = JSON.parse(data);
+                    if (dataJson.name && dataJson.message) {
+                        self.reviewerName(dataJson.name);
+                        self.reviewerMessage(dataJson.message);
+                        console.log(dataJson, self.reviewerName(), self.reviewerMessage());
+                    }
+                }).always(function () {
+                self.isLoading(false);
+            });
+        }
+    });
+});
